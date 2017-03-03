9a1f27e4ac6c0618256aac7c8b056b04bedcac24
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-03 13:41:12 -0500

    27a: additonal scopes added

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
     belongs_to :user
 
     default_scope {order('created_at DESC')}
+    scope :ordered_by_title, -> {order('title DESC')}
+    scope :order_by_reverse_created_at, -> {order('created_at ASC')}
 
     validates :title, length: {minimum: 5, maximum: 100}, presence: true
     validates :body, length: {minimum: 20}, presence: true
