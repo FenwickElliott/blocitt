a06605d15e5e4ab5fa449fd1dc609fc0fb8e28e9
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-03 15:24:27 -0500

    everything reformatted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,61 +1,60 @@
 class PostsController < ApplicationController
 
-    before_action :requre_sign_in, expect: :show
-  def show
-      @post = Post.find(params[:id])
-  end
+before_action :requre_sign_in, expect: :show
 
-  def new
-	  @topic = Topic.find(params[:topic_id])
-      @post = Post.new
-  end
-
-  def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.build(post_params)
-    @post.user = current_user
-
-    if @post.save
-      flash[:notice] = "Post was saved."
-      redirect_to [@topic, @post]
-    else
-      flash.now[:alert] = "There was an error saving the post. Please try again."
-      render :new
+    def show
+        @post = Post.find(params[:id])
     end
-  end
 
-  def edit
-      @post = Post.find(params[:id])
-  end
+    def new
+        @topic = Topic.find(params[:topic_id])
+        @post = Post.new
+    end
 
-  def update
-      @post = Post.find(params[:id])
-      @post.assign_attributes(post_params)
+    def create
+        @topic = Topic.find(params[:topic_id])
+        @post = @topic.posts.build(post_params)
+        @post.user = current_user
+        if @post.save
+            flash[:notice] = "Post was saved."
+            redirect_to [@topic, @post]
+        else
+            flash.now[:alert] = "There was an error saving the post. Please try again."
+            render :new
+        end
+    end
 
-      if @post.save
-          flash[:notice] = "Post was updated."
-          redirect_to [@post.topic, @post]
-      else
-          flash.now[:alert] = "No Luck"
-          render :edit
-      end
-  end
+    def edit
+        @post = Post.find(params[:id])
+    end
 
-   def destroy
-     @post = Post.find(params[:id])
+    def update
+        @post = Post.find(params[:id])
+        @post.assign_attributes(post_params)
+        if @post.save
+            flash[:notice] = "Post was updated."
+            redirect_to [@post.topic, @post]
+        else
+            flash.now[:alert] = "No Luck"
+            render :edit
+        end
+    end
 
-     if @post.destroy
-       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to @post.topic
-     else
-       flash.now[:alert] = "There was an error deleting the post."
-       render :show
-     end
-   end
+    def destroy
+        @post = Post.find(params[:id])
 
-   private
+        if @post.destroy
+            flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+            redirect_to @post.topic
+        else
+            flash.now[:alert] = "There was an error deleting the post."
+            render :show
+        end
+    end
 
-   def post_params
-       params.requre(:post).permit(:title, :body)
-   end
+    private
+
+    def post_params
+        params.requre(:post).permit(:title, :body)
+    end
 end
