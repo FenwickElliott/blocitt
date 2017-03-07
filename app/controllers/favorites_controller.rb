103bd580000436c0d363dfdebf514103c1c17d39
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-07 15:14:06 -0500

    favorite create method implimented

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+
+    before_action :require_sign_in
+
+    def create
+        post = Post.find(params[:post_id])
+        favorite = current_user.favorites.build(post: post)
+
+        if favorite.save
+            flash[:notice] = "Favorited."
+        else
+            flash[:alert] = "Failed."
+        end
+
+        redirect_to [post.topic, post]
+    end
 end
