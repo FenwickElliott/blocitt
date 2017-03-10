29ac5c63b74cfb0fe671627a08e1e2c8358bb40a
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-10 16:26:25 -0500

    --amend

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,14 @@
 module UsersHelper
-    def has_posts
-        if @user.posts == 0
+    def has_posts?
+        if @user.posts.count > 0
             return true
         else
             return false
         end
     end
 
-    def has_comments
-        if @user.comments == 0
+    def has_comments?
+        if @user.comments.count > 0
             return true
         else
             return false
