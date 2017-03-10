b05d2a03590ee0de7b944b8a6ceb3068f2c720d3
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-10 14:30:23 -0500

    posts/comments render message when nil. Not DRY

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+    def has_posts
+        if @user.posts == 0
+            return true
+        else
+            return false
+        end
+    end
+
+    def has_comments
+        if @user.comments == 0
+            return true
+        else
+            return false
+        end
+    end
 end
